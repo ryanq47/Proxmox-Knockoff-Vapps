e078b80fce4356ea772c4103d3d7b9bc65938de8
Ryan Kleffman <91687869+ryanq47@users.noreply.github.com>
2025-03-26 15:35:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ if __name__ in {"__main__", "__mp_main__"}:
     app.add_static_files(
         "/static", "static"
     )  # Serve files from the 'static' directory, to /static
-    ui.run(storage_secret="URMOM", host="0.0.0.0", show=False, dark=True)
+    ui.run(storage_secret="CHANGE_ME_PLEASE", host="0.0.0.0", show=False, dark=True)
